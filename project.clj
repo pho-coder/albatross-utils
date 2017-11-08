090c4a4a8cf9c12eb3a366f550a41cecb7f58102
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-11-08 22:14:14 +0800

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.autohome.bdp.albatross/albatross-utils "1.0-SNAPSHOT"
+  :description "albatross utils"
+  :url "https://github.com/pho-coder/albatross-utils"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.yaml/snakeyaml "1.16"]
+                 [org.clojure/data.json "0.2.6"]])
---
+++ src/com/autohome/bdp/albatross/util/utils.clj
@@ -0,0 +1,88 @@
+(ns com.autohome.bdp.albatross.util.utils
+  (:import [java.io InputStreamReader IOException]
+           [java.sql Timestamp]
+           [java.text SimpleDateFormat]
+           [java.util UUID]
+           [java.lang.management ManagementFactory RuntimeMXBean]
+           [java.net InetAddress]
+           
+           [org.yaml.snakeyaml Yaml])
+  (:require [clojure.data.json :as json]))
+
+(defn wrap-in-runtime
+  "Wraps an exception in a RuntimeException if needed" 
+  [^Exception e]
+  (if (instance? RuntimeException e)
+    e
+    (RuntimeException. e)))
+
+(defn find-yaml
+  "use Yaml parse conf file. must? for throwing Exception without file"
+  [filename & must?]
+  (let [^Yaml yaml (Yaml.)
+        _resources (.. (Thread/currentThread) getContextClassLoader (getResources filename))]
+    (if-not (.hasMoreElements _resources)
+      (if must?
+        (throw (IOException. (str "resource " filename " is not exists!")))
+        {})
+      (let [resources (loop [ret []]
+                        (if (.hasMoreElements _resources)
+                          (recur (conj ret (.nextElement _resources)))
+                          ret))
+            _ (if (> (count resources) 1)
+                (throw (IOException. (str "found multiple " filename " !"))))
+            parameters (.load yaml (InputStreamReader. (.openStream (first resources))))]
+        (apply conj {} parameters)))))
+
+(defn timestamp2datetime
+  ([^Long timestamp-long ^String date-format]
+     (.format (SimpleDateFormat. date-format) (Timestamp. timestamp-long)))
+  ([timestamp-long]
+     (timestamp2datetime timestamp-long "yyyy-MM-dd HH:mm:ss")))
+
+(defn exception-cause? [klass ^Throwable t]
+  (->> (iterate #(.getCause ^Throwable %) t)
+       (take-while identity)
+       (some (partial instance? klass))
+       boolean))
+
+(defn current-time-millis []
+  (System/currentTimeMillis))
+
+(defn uuid []
+  (str (UUID/randomUUID)))
+
+(defn get-pid
+  "get process id"
+  []
+  (let [runtime (ManagementFactory/getRuntimeMXBean)
+        ;; format: "pid@hostname"
+        name (.getName ^RuntimeMXBean runtime)]
+    (.substring name 0 (.indexOf name "@"))))
+
+(defn string->bytes
+  [^String a-str & {:keys [encode] :or {encode "utf-8"}}]
+  (.getBytes a-str encode))
+
+(defn ^String bytes->string
+  [bytes & {:keys [encode] :or {encode "utf-8"}}]
+  (String. bytes encode))
+
+(defn string->map [^String a-string]
+  (json/read-str a-string))
+
+(defn map->string
+  [a-map]
+  (json/write-str a-map))
+
+(defn bytes->map [bytes]
+  (-> bytes bytes->string string->map))
+
+(defn map->bytes [a-map]
+  (-> a-map map->string string->bytes))
+
+(defn ^String get-ip []
+  (.getHostAddress (InetAddress/getLocalHost)))
+
+(defn ^String get-hostname []
+  (.getHostName (InetAddress/getLocalHost)))
